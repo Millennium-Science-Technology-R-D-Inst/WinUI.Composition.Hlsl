776b701c74cbd6c743c4aac9cbc025905b64f4c0
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-13 04:26:18 +0800

    Make generated native shader headers directly consumable

--- tests/BuildIntegration/main.cpp
+++ tests/BuildIntegration/main.cpp
@@ -1,3 +1,9 @@
+#include "ConsumerSampler.g.h"
+#include "ConsumerMaterializedSampler.g.h"
+
+static_assert(sizeof(g_ConsumerSampler_Shader) > 4);
+static_assert(sizeof(g_ConsumerMaterializedSampler_Shader) > 4);
+
 int main()
 {
     return 0;
