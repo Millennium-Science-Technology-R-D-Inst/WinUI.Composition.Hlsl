52417b698ef1896332c22f436b5630abc07a2d0d
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-13 20:28:24 +0800

    Compile multi-source generated shader headers [actions skip]

--- tests/BuildIntegration/main.cpp
+++ tests/BuildIntegration/main.cpp
@@ -1,8 +1,12 @@
 #include "ConsumerSampler.g.h"
 #include "ConsumerMaterializedSampler.g.h"
+#include "ConsumerMultiSourceColor.g.h"
+#include "ConsumerMultiSourceSampler.g.h"
 
 static_assert(sizeof(g_ConsumerSampler_Shader) > 4);
 static_assert(sizeof(g_ConsumerMaterializedSampler_Shader) > 4);
+static_assert(sizeof(g_ConsumerMultiSourceColor_Shader) > 4);
+static_assert(sizeof(g_ConsumerMultiSourceSampler_Shader) > 4);
 
 int main()
 {
