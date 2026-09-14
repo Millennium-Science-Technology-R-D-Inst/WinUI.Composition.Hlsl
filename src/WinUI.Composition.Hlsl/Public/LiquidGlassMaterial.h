86693e02a3f3066abfec22e9934f4c7ba4a1f022
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-14 22:00:58 +0800

    feat: expose optical liquid glass material parameters

--- src/WinUI.Composition.Hlsl/Public/LiquidGlassMaterial.h
+++ src/WinUI.Composition.Hlsl/Public/LiquidGlassMaterial.h
@@ -12,45 +12,46 @@ namespace winrt::WinUI::Composition::Hlsl::implementation
 		{
 			return m_effect;
 		}
-		float BlurRadius() const
-		{
-			return m_BlurRadius;
-		}
+		float BlurRadius() const { return m_BlurRadius; }
 		void BlurRadius(float value);
-		float RefractionStrength() const
-		{
-			return m_RefractionStrength;
-		}
+		float RefractionStrength() const { return m_RefractionStrength; }
 		void RefractionStrength(float value);
-		float DispersionStrength() const
-		{
-			return m_DispersionStrength;
-		}
+		float DispersionStrength() const { return m_DispersionStrength; }
 		void DispersionStrength(float value);
-		float CornerRadius() const
-		{
-			return m_CornerRadius;
-		}
+		float CornerRadius() const { return m_CornerRadius; }
 		void CornerRadius(float value);
-		float BorderThickness() const
-		{
-			return m_BorderThickness;
-		}
+		float BorderThickness() const { return m_BorderThickness; }
 		void BorderThickness(float value);
-		float HighlightStrength() const
-		{
-			return m_HighlightStrength;
-		}
+		float HighlightStrength() const { return m_HighlightStrength; }
 		void HighlightStrength(float value);
+		float BezelWidth() const { return m_BezelWidth; }
+		void BezelWidth(float value);
+		float GlassThickness() const { return m_GlassThickness; }
+		void GlassThickness(float value);
+		float RefractiveIndex() const { return m_RefractiveIndex; }
+		void RefractiveIndex(float value);
+		float TintOpacity() const { return m_TintOpacity; }
+		void TintOpacity(float value);
+		float Saturation() const { return m_Saturation; }
+		void Saturation(float value);
+		float LightAngle() const { return m_LightAngle; }
+		void LightAngle(float value);
+
 	private:
 		Hlsl::HlslEffectBrush m_effect{ nullptr };
 		Microsoft::UI::Composition::CompositionEffectBrush m_compositionEffect{ nullptr };
 		float m_BlurRadius{ 12.0f };
 		float m_RefractionStrength{ 24.0f };
 		float m_DispersionStrength{ 1.2f };
-		float m_CornerRadius{ 12.0f };
-		float m_BorderThickness{ 1.0f };
-		float m_HighlightStrength{ 0.8f };
+		float m_CornerRadius{ 36.0f };
+		float m_BorderThickness{ 1.5f };
+		float m_HighlightStrength{ 0.85f };
+		float m_BezelWidth{ 32.0f };
+		float m_GlassThickness{ 50.0f };
+		float m_RefractiveIndex{ 1.5f };
+		float m_TintOpacity{ 0.08f };
+		float m_Saturation{ 1.25f };
+		float m_LightAngle{ -0.95f };
 	};
 }
 namespace winrt::WinUI::Composition::Hlsl::factory_implementation
