0107c73ef2d42e4732bc15f2b8d6917db8112d5b
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-14 21:59:03 +0800

    feat: expose optical liquid glass property paths

--- src/WinUI.Composition.Hlsl/Materials/CustomLiquidGlassEffect.h
+++ src/WinUI.Composition.Hlsl/Materials/CustomLiquidGlassEffect.h
@@ -11,6 +11,12 @@ namespace CustomLiquidGlassEffect
 	inline constexpr wchar_t BorderThicknessPropertyPath[] = L"BackdropLiquidGlassEffect.BorderThickness";
 	inline constexpr wchar_t HighlightStrengthPropertyPath[] = L"BackdropLiquidGlassEffect.HighlightStrength";
 	inline constexpr wchar_t DispersionStrengthPropertyPath[] = L"BackdropLiquidGlassEffect.DispersionStrength";
+	inline constexpr wchar_t BezelWidthPropertyPath[] = L"BackdropLiquidGlassEffect.BezelWidth";
+	inline constexpr wchar_t GlassThicknessPropertyPath[] = L"BackdropLiquidGlassEffect.GlassThickness";
+	inline constexpr wchar_t RefractiveIndexPropertyPath[] = L"BackdropLiquidGlassEffect.RefractiveIndex";
+	inline constexpr wchar_t TintOpacityPropertyPath[] = L"BackdropLiquidGlassEffect.TintOpacity";
+	inline constexpr wchar_t SaturationPropertyPath[] = L"BackdropLiquidGlassEffect.Saturation";
+	inline constexpr wchar_t LightAnglePropertyPath[] = L"BackdropLiquidGlassEffect.LightAngle";
 
 	std::shared_ptr<hlsl::engine::EffectDefinition const> Description();
 	winrt::Windows::Graphics::Effects::IGraphicsEffect CreateEffect();
