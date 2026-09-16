82e5e5521d0b10fa68e383b36e68ffddd03eb723
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-17 03:50:18 +0800

    centralize liquid glass preset creation

--- src/WinUI.LiquidGlass/LiquidGlassControls.cpp
+++ src/WinUI.LiquidGlass/LiquidGlassControls.cpp
@@ -19,21 +19,14 @@
 
 namespace winrt::WinUI::LiquidGlass::implementation
 {
-    using namespace std::chrono_literals;
     namespace
     {
         namespace Xaml = Microsoft::UI::Xaml;
         namespace Controls = Xaml::Controls;
-        namespace Primitives = Controls::Primitives;
         namespace Media = Xaml::Media;
-        namespace Shapes = Xaml::Shapes;
 
         using Brush = WinUI::Composition::Hlsl::LiquidGlassBrush;
-        using Profile = WinUI::Composition::Hlsl::LiquidGlassSurfaceProfile;
-        using Xaml::DependencyObject;
-        using Xaml::FrameworkElement;
-
-        enum class Preset { Panel, Button, Choice, Search, Input, Slider, Switch, Magnifier };
+        using Preset = WinUI::LiquidGlass::LiquidGlassPreset;
 
         Media::Brush AsBrush(Brush const& value)
         {
@@ -42,66 +35,7 @@ namespace winrt::WinUI::LiquidGlass::implementation
 
         Brush CreateBrush(Preset preset)
         {
-            Brush b;
-            b.SurfaceProfile(Profile::ConvexSquircle);
-            b.BlurRadius(1.0);
-            b.Saturation(1.0);
-            b.Contrast(1.0);
-            b.Exposure(0.0);
-            b.MaterialOpacity(1.0);
-            b.EdgeSoftness(1.0);
-
-            switch (preset)
-            {
-            case Preset::Panel:
-                b.CornerRadius(31); b.RefractionStrength(24); b.DispersionStrength(.45);
-                b.BezelWidth(29); b.GlassThickness(90); b.RefractiveIndex(1.3);
-                b.HighlightStrength(.4); b.HighlightSharpness(1.6); b.SpecularSaturation(6);
-                b.SpecularWidth(1); b.TintOpacity(.12); b.InnerShadowStrength(.05);
-                b.FallbackColor({ 0x55, 0xff, 0xff, 0xff }); break;
-            case Preset::Button:
-            case Preset::Choice:
-                b.CornerRadius(preset == Preset::Choice ? 10 : 8);
-                b.RefractionStrength(18); b.DispersionStrength(.55);
-                b.BezelWidth(preset == Preset::Choice ? 9 : 12);
-                b.GlassThickness(preset == Preset::Choice ? 32 : 48);
-                b.RefractiveIndex(1.45); b.HighlightStrength(.4); b.HighlightSharpness(1.8);
-                b.SpecularSaturation(5); b.SpecularWidth(1); b.TintOpacity(.12);
-                b.InnerShadowStrength(.06); b.FallbackColor({ 0x44, 0xff, 0xff, 0xff }); break;
-            case Preset::Search:
-                b.CornerRadius(28); b.RefractionStrength(16.8); b.DispersionStrength(.35);
-                b.BezelWidth(27); b.GlassThickness(70); b.RefractiveIndex(1.5);
-                b.HighlightStrength(.2); b.HighlightSharpness(1.7); b.SpecularSaturation(4);
-                b.SpecularWidth(1); b.TintOpacity(.05); b.InnerShadowStrength(.04);
-                b.FallbackColor({ 0x30, 0xff, 0xff, 0xff }); break;
-            case Preset::Input:
-                b.CornerRadius(8); b.RefractionStrength(16); b.DispersionStrength(.4);
-                b.BezelWidth(14); b.GlassThickness(60); b.RefractiveIndex(1.45);
-                b.HighlightStrength(.3); b.HighlightSharpness(1.8); b.SpecularSaturation(4);
-                b.SpecularWidth(1); b.TintOpacity(.10); b.InnerShadowStrength(.05);
-                b.FallbackColor({ 0x36, 0xff, 0xff, 0xff }); break;
-            case Preset::Slider:
-                b.CornerRadius(30); b.BlurRadius(0); b.RefractionStrength(9.6);
-                b.DispersionStrength(.45); b.BezelWidth(16); b.GlassThickness(80);
-                b.RefractiveIndex(1.45); b.HighlightStrength(.4); b.HighlightSharpness(1.7);
-                b.SpecularSaturation(7); b.SpecularWidth(1); b.TintOpacity(1);
-                b.InnerShadowStrength(.05); b.FallbackColor({ 0xff, 0xff, 0xff, 0xff }); break;
-            case Preset::Switch:
-                b.SurfaceProfile(Profile::Lip); b.CornerRadius(46); b.BlurRadius(.2);
-                b.RefractionStrength(9.6); b.DispersionStrength(.45); b.BezelWidth(19);
-                b.GlassThickness(47); b.RefractiveIndex(1.5); b.HighlightStrength(.5);
-                b.HighlightSharpness(1.6); b.SpecularSaturation(6); b.SpecularWidth(1);
-                b.TintOpacity(1); b.InnerShadowStrength(.02);
-                b.FallbackColor({ 0xff, 0xff, 0xff, 0xff }); break;
-            case Preset::Magnifier:
-                b.CornerRadius(75); b.BlurRadius(0); b.RefractionStrength(19.2);
-                b.DispersionStrength(.55); b.BezelWidth(25); b.GlassThickness(110);
-                b.RefractiveIndex(1.5); b.MagnificationStrength(24); b.HighlightStrength(.5);
-                b.HighlightSharpness(1.6); b.SpecularSaturation(9); b.SpecularWidth(1);
-                b.TintOpacity(.02); b.InnerShadowStrength(.20);
-                b.FallbackColor({ 0x28, 0xff, 0xff, 0xff }); break;
-            }
-            return b;
+            return WinUI::LiquidGlass::LiquidGlassPresets::CreateBrush(preset);
         }
     }
 
@@ -213,7 +147,7 @@ namespace winrt::WinUI::LiquidGlass::implementation
     LiquidGlassSlider::LiquidGlassSlider()
     {
         DefaultStyleKey(box_value(xaml_typename<class_type>()));
-        GlassBrush(CreateBrush(Preset::Slider));
+        GlassBrush(CreateBrush(Preset::SliderThumb));
         SetValue(LiquidGlassInteraction::RestScaleProperty(), box_value(.6));
         SetValue(LiquidGlassInteraction::PressedScaleProperty(), box_value(1.0));
         SetValue(LiquidGlassInteraction::MotionDurationProperty(), box_value(120.0));
@@ -228,7 +162,7 @@ namespace winrt::WinUI::LiquidGlass::implementation
     LiquidGlassToggleSwitch::LiquidGlassToggleSwitch()
     {
         DefaultStyleKey(box_value(xaml_typename<class_type>()));
-        GlassBrush(CreateBrush(Preset::Switch));
+        GlassBrush(CreateBrush(Preset::ToggleSwitchKnob));
         SetValue(LiquidGlassInteraction::RestScaleProperty(), box_value(.65));
         SetValue(LiquidGlassInteraction::PressedScaleProperty(), box_value(.9));
         SetValue(LiquidGlassInteraction::MotionDurationProperty(), box_value(130.0));
